054969d69351000fa54091e29ecb657eb0df5b78
tk <tk@kamakuchikeita-no-MacBook-Pro.local>
2017-12-23 21:31:08 +0900

    三項演算子

--- t02.php
+++ t02.php
@@ -9,3 +9,15 @@ echo $jjj;
 $hh = "OK just do it!";
 }
 echo $hh;
+
+$mes = "0";
+
+echo $mes > 9 ? $mes : 'から';
+
+$x = "いつもここから";
+
+$x === 0 ? -1 : $flag = $x;
+
+echo $flag;
+
+//echo true ? 0 : (false ? 'いいよ': 'NG');
