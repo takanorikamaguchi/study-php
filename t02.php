6dd43c8764c8dde6f64bbcb59b975c4fbbf0de7f
takanori <takanori@takanori-no-MacBook-Pro.local>
2017-12-24 16:02:33 +0900

    php配列系学習

--- t02.php
+++ t02.php
@@ -20,7 +20,20 @@ $x === 0 ? -1 : $flag = $x;
 
 echo $flag;
 
-//echo true ? 0 : (false ? 'いいよ': 'NG');
+//echo true ? 0 : (false ? 'いいよ': 'NG
+
+$date = ['1富士','3宍戸','2棚','近藤','上智','田中'];
+$date2 = [
+	['富士','宍戸','棚','近藤','上智','田中'],'棚','近藤','上智','田中'];
+
+echo 'string';
+
+echo "<br>".array_search('宍戸', $date);
+var_dump(array_search(12, $date, true));
+
+sort($date, SORT_NATURAL);
+print_r($date);
+
 ?>
 
 <!DOCTYPE html>
